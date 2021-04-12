effd5bc39bfd9ef8a6832694d3d63590a21994e4
goueguy <jlagoueguy@gmail.com>
2021-04-12 22:00:32 +0000

    Make use of asset function help to load images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section("title",config("app.name"))
 @section('content')
-    <img src="/images/1200px-Flag_of_Quebec.png" alt="Québec flag" style="width:200px">
+    <img src="{{asset('images/1200px-Flag_of_Quebec.png')}}" alt="Québec flag" style="width:200px">
     <h1>HELLO FROM QUEBEC!</h1>
     <p>It's currently {{ date("H:i")}}.</p>
 
