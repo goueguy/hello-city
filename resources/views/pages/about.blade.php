7798f9eb09c9e68f3c78495884ec75b7ef37e056
goueguy <jlagoueguy@gmail.com>
2021-04-12 16:04:43 +0000

    remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>About | Hello City</title>
-    </head>
-    <body>
-        
-        <p>Built with &hearts; by JlGoueguy.</p>
-        <p><a href="/">Revenir à la page d'accueil</a></p>
-        <footer>
-            <p>&copy; Copyright {{ date("Y")}} &middot; <a href="/about-us">About US</p>
-        </footer>
-    </body>
-</html>
+@extends('app')
+
+@section('content')
+    <p>Built with &hearts; by JlGoueguy.</p>
+    <p><a href="/">Revenir à la page d'accueil</a></p>
+@endsection
