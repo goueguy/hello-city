3cbfe63f96618311cf6ca180c1dd23c33c363751
goueguy <jlagoueguy@gmail.com>
2021-04-12 20:52:29 +0000

    add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 @section("title","About US | ".config("app.name"))
 @section('content')
+    <img src="/images/tdn-logo.png" alt="TDN Logo">
     <p>Built with &hearts; by JlGoueguy.</p>
     <p><a href="{{route('home')}}">Revenir à la page d'accueil</a></p>
 @endsection
